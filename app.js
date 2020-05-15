8e654d7104b1577e583b28e6ab702bac6cc7eba3
roadstobe <epro100live@gmail.com>
2020-05-15 12:39:48 +0300

    'createApi'

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ require('./middleware/passport')(passport);
 
 //URL PARSER
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
--- controllers/category.js
+++ controllers/category.js
@@ -1,22 +1,69 @@
-module.exports.getAll = (req, res) =>{
-    res.json({
-        message: 'Category'
+const Category = require('../models/Category');
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
+
+module.exports.getAll = async (req, res) =>{
+    try{
+        const categories = await Category.find({user: req.user.id});
+        res.status(200).json(categories);
+    }catch (e) {
+        errorHandler(res, e)
+    }
+}
+
+module.exports.getById = async (req, res) =>{
+    try{
+        const category = await Category.findById(req.params.id);
+        res.status(200).json(category);
+    }catch (e) {
+        errorHandler(res, e)
+    }
+}
+
+module.exports.remove = async (req, res) =>{
+    try{
+        await Category.remove({_id: req.params.id});
+        await Position.remove({category: req.params.id});
+        res.status(200).json({message: 'Category removed'})
+    }catch (e) {
+        errorHandler(res, e)
+    }
+}
+
+module.exports.create = async (req, res) =>{
+
+    const category = new Category({
+        name: req.body.name,
+        user: req.user.id,
+        imageSrc: req.file ? req.file.path : ''
     })
+
+    try{
+        await category.save();
+        res.status(201).json(category);
+    }catch (e) {
+        errorHandler(res, e)
+    }
 }
 
-module.exports.getById = (req, res) =>{
+module.exports.update = async (req, res) =>{
+    const updated = {
+        name: req.body.name
+    };
+    if(req.file){
+        updated.imageSrc = req.file.path;
+    }
 
-}
+    try{
+        const category = await Category.findOneAndUpdate(
+            {_id: req.params.id},
+            {$set: updated},
+            {new: true}
+            );
 
-module.exports.remove = (req, res) =>{
-
-}
-
-module.exports.create = (req, res) =>{
-
-}
-
-module.exports.update = (req, res) =>{
-
+        res.status(200).json(category);
+    }catch (e) {
+        errorHandler(res, e)
+    }
 }
 
